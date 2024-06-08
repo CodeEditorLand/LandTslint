21787bc39b7bfc7d3b559d20aebd1458a72a2f29
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-08 07:34:30 +0300

    squash!

--- tslint-tests/tests/selective-auto-fix-on-save.ts
+++
@@ -1,6 +0,0 @@
-// Only the 'arrow-parens' rule should be fixed, the 'quotemark' failure
-// should not be fixed
-//    "tslint.autoFixOnSave": ["arrow-parens"],
-
-[1, 2].map(num => console.log('test: ' + num));
-
